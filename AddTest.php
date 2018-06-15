3521d120fc035f91c8c41f1bfdff39ab49cea7f2
amod1kore <36945488+amod1kore@users.noreply.github.com>
2018-06-15 10:40:44 +0530

    Add files via upload

---
+++ AddTest.php
@@ -0,0 +1,123 @@
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Online Test</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Bootstrap CSS
+   ================================================== -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Animate CSS
+   ================================================== -->
+    <link rel="stylesheet" href="css/animate.min.css">
+
+    <!-- Font Icons
+   ================================================== -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/AddTest.css">
+
+
+    <!-- Google web font
+   ================================================== -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+       
+</head>
+
+<body>
+    <div class="container">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 header">
+    </div>
+    <div class="col-md-12 col-lg-12 well Maindiv ">
+            <form>
+               <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1">
+                 <textarea class="form-control" rows="5"></textarea>
+                   
+              </div>
+               
+              <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1 option">
+                   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3">A Ans</label>
+                              <div class="col-md-9">
+                              <input type="text" class="form-control" name="firstopt" placeholder="First option">
+                            </div>
+                        </div>                  
+                   </div>
+                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3">B Ans </label>
+                              <div class="col-md-9">
+                              <input type="text" class="form-control" name="secondopt" placeholder="Second option">
+                            </div>
+                        </div>                  
+                   </div>
+              </div>
+            <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1 option">
+                   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3">C Ans</label>
+                              <div class="col-md-9">
+                              <input type="text" class="form-control" name="firstopt" placeholder="third option">
+                            </div>
+                        </div>                  
+                   </div>
+                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3">D Ans </label>
+                              <div class="col-md-9">
+                              <input type="text" class="form-control" name="secondopt" placeholder="fourth option">
+                            </div>
+                        </div>                  
+                   </div>
+              </div>
+              <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1 option">
+                   <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3"> Ans</label>
+                              <div class="col-md-9">
+                              <input type="text" class="form-control" name="firstopt" placeholder="Ans option">
+                            </div>
+                        </div>                  
+                   </div>
+                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div class="form-group">
+                              <label class="col-md-3"></label>
+                              <div class="col-md-9">
+                             <input type="submit" name="submit" value="submit" id="submit" class="btn btn-primary col-md-12"/>
+                            </div>
+                        </div> 
+                   </div>
+              </div>
+            </form>
+    </div>
+</div>
+
+    <!-- Javascript 
+================================================== -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/custom.js"></script>
+    
+<!--
+     <script>
+            $(document).ready(function(){
+              $('#submit').click(function(){
+                 $(this).parents('.Maindiv').addClass("fadeOutRight");
+              });
+            });
+        </script>
+-->
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,93 @@
+<html>
+ <head>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Online Test</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--
+
+Template 2078 Adventure
+
+http://www.tooplate.com/view/2078-adventure
+
+-->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Bootstrap CSS
+   ================================================== -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<!-- Animate CSS
+   ================================================== -->
+	<link rel="stylesheet" href="css/animate.min.css">
+
+	<!-- Font Icons
+   ================================================== -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href="css/Login.css">
+     
+
+	<!-- Google web font
+   ================================================== -->
+	<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+	
+</head>
+
+<body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+   <div class="col-xs-12 col-sm-12 col-lg-12 col-md-12">
+       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 images">
+       </div>
+      
+       <div class="col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4 col-sm-10 col-xs-10 box">
+          <div class="col-xs-12 col-ms-12 col-md-12 col-lg-12 Login_box">              
+              <form class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 form1">
+                   <br/>
+                   <input type="text" class="form-control" placeholder="username">
+                   <br/>
+                   <input type="text" class="form-control" placeholder="password">
+                  <br/>
+                  <input type="submit" class="btn btn-clock btn-primary col-md-12" value="submit"/>
+                  <br/>
+                  <br/>
+                  <b>forget Password ?</b>
+                  <br/>
+                  <hr/>
+                  <div class="take-exam" style="">
+									<a href="registration1.php">Applicant Student Registration »</a><br>
+<!--
+		
+									<a href="registration1?flag=1">Applicant Employee Registration »</a><br>
+			
+								<a href="alumniRegistration.htm">Alumni Registration
+									»</a><br>
+-->
+							</div>
+              </form>
+           </div>
+              
+          
+
+       </div>
+  </div>
+    
+<!-- Javascript 
+================================================== -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/nivo-lightbox.min.js"></script>
+<script src="js/jquery.easing-1.3.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
